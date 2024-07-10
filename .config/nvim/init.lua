ca994345ac20af5afdd798f8684b091a0cdf6a48
Jonny Loughlin <jonny@jloughlin.dev>
2024-07-10 12:10:32 -0400

    minor changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -40,6 +40,11 @@ vim.opt.splitright = true
 
 vim.opt.completeopt = { "menuone", "noselect" }
 
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "templ",
     callback = function()
--- .config/nvim/lua/plugins/better-ts-error.lua
+++ .config/nvim/lua/plugins/better-ts-error.lua
@@ -2,7 +2,7 @@ return {
     "OlegGulevskyy/better-ts-errors.nvim",
     ft = { "javascript", "javascripttreact", "typescript", "typescriptreact" },
     dependencies = { "MunifTanjim/nui.nvim" },
-    config = {
+    opts = {
         keymaps = {
             toggle = "<leader>dd", -- default '<leader>dd'
             go_to_definition = "<leader>dx", -- default '<leader>dx'
