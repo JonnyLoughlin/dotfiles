af4db12c33592930731d3eb5a5017e79928ce2a8
Jonny Loughlin <jonny@jloughlin.dev>
2024-07-24 10:09:08 -0400

    upates

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -24,8 +24,6 @@ return {
             bash = { "shfmt" },
             sh = { "shfmt" },
 
-            sql = { "sql_formatter" },
-
             templ = { "templ" },
 
             zsh = { "shfmt" },
