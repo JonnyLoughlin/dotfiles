9efe2c1d48b6a7bb0b32b6ef416a26cadbb96445
Jonny Loughlin <jonny@jloughlin.dev>
2024-08-09 11:32:04 -0400

    floating window rules and nvim lsp and which-key updates

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,14 +1,4 @@
 return {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    keys = {
-        { "<leader>c", group = "Comment" },
-        { "<leader>g", group = "Gitsigns" },
-        { "<leader>o", group = "Oil" },
-        { "<leader>p", group = "Peek" },
-        { "<leader>v", group = "Go Snippets" },
-        { "<leader>x", group = "Conform" },
-        { "<leader>y", group = "You Surround" },
-        { "K", group = "LSP" },
-    },
 }
