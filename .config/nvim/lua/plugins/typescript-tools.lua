f0ef8bcce836d3985baec2c5f7ad214b94a28b8d
Jonny Loughlin <jonny@jloughlin.dev>
2024-08-29 08:43:51 -0400

    zen browser

--- .config/nvim/lua/plugins/typescript-tools.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "pmizio/typescript-tools.nvim",
-    ft = { "javascript", "javascripttreact", "typescript", "typescriptreact" },
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
-}
