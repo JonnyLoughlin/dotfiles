b8d99004550170f345a6be98e495e8ca01134c63
Jonny Loughlin <jonny@jloughlin.dev>
2024-09-06 11:19:33 -0400

    updates: tailwind fix, hyprland sleep testing

--- .config/nvim/lua/plugins/tailwind-sorter.lua
+++ .config/nvim/lua/plugins/tailwind-sorter.lua
@@ -1,10 +1,27 @@
+-- tailwind-tools.lua
 return {
-    "laytan/tailwind-sorter.nvim",
-    ft = { "javascript", "javascripttreact", "typescript", "typescriptreact", "html", "templ" },
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
-    build = "cd formatter && npm ci && npm run build",
-    opts = {
-        on_save_enabled = true,
-        on_save_pattern = { "*.html", "*.jsx", "*.tsx", "*.templ" },
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-telescope/telescope.nvim",
+        "neovim/nvim-lspconfig",
     },
+    ft = { "html", "templ" },
+    keys = {
+        { "<leader>tt", "<cmd>TailwindConcealToggle<cr>", { desc = "toggle tailwind conceal" } },
+    },
+    config = function()
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.templ",
+            callback = function() require("tailwind-tools.lsp").sort_classes(false) end,
+        })
+
+        require("tailwind-tools").setup({
+            document_color = {
+                inline_symbol = "󰝤",
+            },
+        })
+    end,
 }
