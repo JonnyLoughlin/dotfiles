b27d5f10e7b98c76d9c5aac425ad50bb9999b989
Jonny Loughlin <jonny@jloughlin.dev>
2024-11-15 10:45:07 -0500

    Update git hook instructions

--- .config/scripts/git-hooks/setup-git-hooks.sh
+++ .config/scripts/git-hooks/setup-git-hooks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Setting up package tracking pre-commit hooks..." >&2
 mkdir -p ~/.dotfiles/hooks
 cp ~/.config/scripts/git-hooks/pre-commit ~/.dotfiles/hooks/
 chmod +x ~/.dotfiles/hooks/pre-commit
